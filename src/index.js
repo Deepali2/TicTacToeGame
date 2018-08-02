5ffd4fea8cf45994fe8dc61545492124fa9e9619
Deepali2 <hdgarg@gmail.com>
2018-08-01 21:44:27 -0400

    raises the state to the board component; renders an X for each square that is clicked

--- src/index.js
+++ src/index.js
@@ -2,32 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+const Square = (props) => {
+    return (
+        <button className="square" onClick={props.handleClick}>
+        {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-          value:null,
+          squares:Array(9).fill(null),
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick() {
-        this.setState({value: 'X'});
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
         
     }
-    
-    render() {
-        return (
-          <button className="square" onClick={this.handleClick}>
-            {this.state.value}
-          </button>
-        );
-    }
-}
 
-class Board extends React.Component {
     renderSquare(i){
-        return <Square value={i}/>;
+        return <Square value={this.state.squares[i]} handleClick={() => this.handleClick(i)} />;
     }
 
     render() {
@@ -36,11 +36,13 @@ class Board extends React.Component {
           <div>
               <div className="status">{status}</div>
               <div className="board-row">
+              
                {this.renderSquare(0)}
                {this.renderSquare(1)}
                {this.renderSquare(2)}
               </div>
               <div className="board-row">
+             
                {this.renderSquare(3)}
                {this.renderSquare(4)}
                {this.renderSquare(5)}
